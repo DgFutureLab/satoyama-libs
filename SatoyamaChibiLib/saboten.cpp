641793f771f7d213ee50fc516921e47c36b1efbc
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-02-10 14:36:34 +0900

    Adds saboten class

---
+++ SatoyamaChibiLib/saboten.cpp
@@ -0,0 +1,38 @@
+#include "saboten.h"
+
+/*
+IN H FILE:
+
+PCF2127(uint8_t devAddr, uint8_t mode, uint8_t i2c, uint8_t cs, uint8_t *temp);
+void writeTime(uint8_t hour, uint8_t minutes, uint8_t seconds);
+
+
+IN CPP FILE:
+
+CONSTRUCTOR:
+
+PCF2127::PCF2127(uint8_t devAddr, uint8_t mode, uint8_t i2c, uint8_t cs, uint8_t *temp)
+{
+   
+}
+
+
+void PCF2127::writeTime(uint8_t hours, uint8_t minutes, uint8_t seconds)
+{
+ 
+}
+
+*/
+
+
+Saboten::Saboten(){
+
+}
+
+void Saboten::sleep_mcu(){
+
+}
+
+void Saboten::sleep_radio(){
+
+}
---
+++ SatoyamaChibiLib/saboten.h
@@ -0,0 +1,31 @@
+/*
+IN H FILE:
+
+PCF2127(uint8_t devAddr, uint8_t mode, uint8_t i2c, uint8_t cs, uint8_t *temp);
+void writeTime(uint8_t hour, uint8_t minutes, uint8_t seconds);
+
+
+IN CPP FILE:
+
+CONSTRUCTOR:
+
+PCF2127::PCF2127(uint8_t devAddr, uint8_t mode, uint8_t i2c, uint8_t cs, uint8_t *temp)
+{
+   
+}
+
+
+void PCF2127::writeTime(uint8_t hours, uint8_t minutes, uint8_t seconds)
+{
+ 
+}
+
+*/
+
+
+class Saboten{
+	public:
+		Saboten();
+		void sleep_mcu();
+		void sleep_radio();
+};
