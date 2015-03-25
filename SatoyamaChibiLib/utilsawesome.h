1a57bb56147cab0a46b9d60b1cc1f21845a3131e
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-03-25 17:43:07 +0900

    Adds replacement add_to_tx_buffer which removes the need for casting

--- SatoyamaChibiLib/utilsawesome.h
+++ SatoyamaChibiLib/utilsawesome.h
@@ -10,6 +10,7 @@ typedef struct Reading{
 };
 
 void add_to_tx_buf(char *tx_buf, struct Reading *reading);
+void add_to_tx_buf_new(unsigned char *tx_buf, struct Reading *reading);
 
 #ifdef __cplusplus
 }
