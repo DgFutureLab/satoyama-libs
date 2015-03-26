6468ccce8b96e000017a9e6ab92def29308c8e81
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-03-27 00:37:14 +0900

    shit compiles

--- SatoyamaChibiLib/sensors.cpp
+++ SatoyamaChibiLib/sensors.cpp
@@ -1,7 +1,10 @@
+#include <vector>
 #include "sensors.h"
 #include <chibi.h>
 #include <utilsawesome.h>
-#include <saboten.h>
+// #include <saboten.h>
+
+
 
 void Sensors::read_battery_voltage(unsigned char *buffer, int battery_voltage_pin, float reference_voltage){
   unsigned int vbat = analogRead(battery_voltage_pin);
@@ -9,3 +12,75 @@ void Sensors::read_battery_voltage(unsigned char *buffer, int battery_voltage_pi
   Reading battery_voltage = {"vbat", batt, millis()};
   add_to_tx_buf_new(buffer, &battery_voltage);
 }
+
+
+// void read_Paralax28015REVC(unsigned char *buffer, int sonar_pin, NewPing sonar){
+// 	/*
+// 	Conmf
+// 	*/
+// 	float distance = sonar.ping() / US_ROUNDTRIP_CM; 
+
+//     if (distance > 0) {
+//       Reading dist = {"distance", distance, millis()};
+//       add_to_tx_buf_new((char*)tx_buf, &dist);
+//     }	
+// }
+
+// std::vector<BaseSensor> 
+
+
+class BaseSensor{
+	public: 
+		virtual void read_sensors(unsigned char *buffer) = 0;
+};
+
+class Board{
+	private:
+		int n_sensors;
+		BaseSensor *sensors[];
+		std::vector<int> *humhum; 
+
+	public:
+		Board(int total_sensors): n_sensors(0){
+			// sensors = new BaseSensor[10];
+			this->humhum = new std::vector<int>(10);
+
+		}
+
+		void register_sensor(BaseSensor *sensor){
+			n_sensors++;
+			int hum;
+
+		}
+
+};
+
+
+BaseSensor *sensors[10];
+
+#include <NewPing.h>
+class PCF2127Sensor:public BaseSensor{
+	private: 
+		NewPing *sonar; // Sonar is a pointer that points to a NewPing instance
+	
+	public:
+		PCF2127Sensor(int sonar_pin){
+			sonar = new NewPing(sonar_pin, sonar_pin, 200);
+		};
+
+		float read_values(unsigned char *buffer){
+			float distance = (float) sonar->ping() / US_ROUNDTRIP_CM; 
+			// float distance = this->sonar.ping() / 12; 
+		}
+};
+
+
+// class DHT11Sensor:public BaseSensor{
+
+// 	PCF2127Sensor(){
+// 		DHT temp(0, 0, 0, RTC_CHIPSELECT_PIN, &temp);
+	
+
+// 	}
+// };
+
--- SatoyamaChibiLib/sensors.h
+++ SatoyamaChibiLib/sensors.h
@@ -1,4 +1,4 @@
-
+#include <NewPing.h>
 class Sensors{
 	/*
 	This class does not really represent a sensor, but it's
@@ -7,4 +7,5 @@ class Sensors{
 	*/
 	public: 
 		static void read_battery_voltage(unsigned char *buffer, int battery_voltage_pin, float reference_voltage);
+		static void read_Paralax28015REVC(unsigned char *buffer, int sonar_pin, NewPing sonar);
 };
